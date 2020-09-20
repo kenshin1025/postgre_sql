41466ea51033343bff3e78498652792a80b74d53
kenshin1025 <sukoyakamind1025@gmail.com>
2020-09-20 15:23:41 +0900

    実行時間を計測できるようにした

--- go/main.go
+++ go/main.go
@@ -15,15 +15,27 @@ type User struct {
 	Name string
 }
 
-func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World! %s", time.Now())
+func OpenDB() (*gorm.DB, error) {
 	dsn := "host=db user=admin password=admin dbname=admin port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
 	}
+	return db, err
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	db, err := OpenDB()
+	if err != nil {
+		log.Fatal(err)
+	}
 	var user User
+	stratTime := time.Now()
+	fmt.Fprintf(w, "%s\n", stratTime)
 	db.Take(&user)
+	endTime := time.Now()
+	fmt.Fprintf(w, "%s\n", endTime)
+	fmt.Fprintf(w, "%s\n", endTime.Sub(stratTime))
 
 	fmt.Println(user.Name)
 }
